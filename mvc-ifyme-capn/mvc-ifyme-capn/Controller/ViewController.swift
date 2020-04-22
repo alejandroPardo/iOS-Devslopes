e54e094e5fb26b8b2c084c4ee439af4672fecd36
Alejandro Pardo Rodriguez <alejandropro@ext.inditex.com>
2020-04-22 12:39:13 +0200

    Creates application and Model class

---
+++ mvc-ifyme-capn/mvc-ifyme-capn/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvc-ifyme-capn
+//
+//  Created by Alejandro Pardo on 22/04/2020.
+//  Copyright © 2020 Alejandro Pardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvc-ifyme-capn/mvc-ifyme-capn/Model/AppleProduct.swift
@@ -0,0 +1,20 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-capn
+//
+//  Created by Alejandro Pardo on 22/04/2020.
+//  Copyright © 2020 Alejandro Pardo. All rights reserved.
+//
+
+import Foundation
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
